f17abbf5a88f476a9d8e1ee47167d1b343e2d30a
DevRenato <u18215194@gmail.com>
2023-02-28 16:02:57 -0500

    Estilos al Modal

--- src/components/calendar/CalendarModal.jsx
+++ src/components/calendar/CalendarModal.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 import ReactModal from "react-modal";
 import DatePicker, { registerLocale } from "react-datepicker";
-import { addHours } from "date-fns";
+import { addHours, differenceInSeconds } from "date-fns";
 import es from 'date-fns/locale/es';
 
 import "react-datepicker/dist/react-datepicker.css";
@@ -21,7 +21,13 @@ const customStyles = {
 ReactModal.setAppElement("#root");
 
 export const CalendarModal = () => {
+
   const [isOpen, setisOpen] = useState(true);
+  const [ formValid, setFormValid ] = useState({
+    date: false,
+    title: false,
+    note: false
+  });
   const [ formValues, setFormValues ] = useState({
     title: "Evento",
     notes: "Eventos",
@@ -47,32 +53,48 @@ export const CalendarModal = () => {
     });
   }
 
+  const handleSubmit = ( e ) => {
+    e.preventDefault();
+    
+    const difference = differenceInSeconds( formValues.end, formValues.start );
+    
+    if ( difference < 0 || difference === 0 || isNaN(difference) ) return setFormValid({date: true});
+    if ( formValues.title.length === 0 ) return setFormValid({title: true});
+    if ( formValues.title.length === 0 ) return setFormValid({note: true});
+    
+    setFormValid({})
+  }
+
   return (
     <ReactModal
       isOpen={isOpen}
       onRequestClose={closeModal}
       style={customStyles}
-      overlayClassName="modal-fondo"
+      overlayClassName="Modal__fondo"
+      className='Modal__container'
       closeTimeoutMS={200}
     >
-      <h1> Nuevo evento </h1>
-      <hr />
-      <form className="container">
-        <div className="form-group mb-2">
-          <label>Fecha y hora inicio</label>
+      <h1 className="Modal__title"> Nuevo evento </h1>
+      <form onSubmit={ handleSubmit } className="Modal__form">
+        <div className="Modal__group">
+          <label htmlFor="date-start" className="Modal__label-date">Fecha y hora inicio</label>
           <DatePicker
+            id="date-start"
             locale="es"
+            maxDate={ formValues.end }
             selected={ formValues.start }
             onChange={ (e) => handleDateChanged( e, 'start' ) }
             showTimeSelect
             dateFormat="Pp"
             timeCaption="Hora"
+            className="Modal__input"
           />
         </div>
 
-        <div className="form-group mb-2">
-          <label>Fecha y hora fin</label>
+        <div className={`Modal__group ${ formValid.date && 'Modal__group-error' }`} >
+          <label htmlFor="date-end" className="Modal__label-date">Fecha y hora fin</label>
           <DatePicker
+            id="date-end"
             locale="es"
             minDate={ formValues.start }
             selected={ formValues.end }
@@ -80,42 +102,42 @@ export const CalendarModal = () => {
             showTimeSelect
             dateFormat="Pp"
             timeCaption="Hora"
+            className="Modal__input"
           />
         </div>
 
-        <hr />
-        <div className="form-group mb-2">
-          <label>Titulo y notas</label>
+        <div className={`Modal__group ${ formValid.title && 'Modal__group-error' }`} >
           <input
+            id="title"
             type="text"
-            className="form-control"
-            placeholder="Título del evento"
+            className="Modal__input"
+            placeholder=" "
             name="title"
             autoComplete="off"
             value={formValues.title}
             onChange={ handleInputChange }
           />
-          <small id="emailHelp" className="form-text text-muted">
-            Una descripción corta
-          </small>
+          <label htmlFor="title" className="Modal__label">Titulo y notas</label>
         </div>
 
-        <div className="form-group mb-2">
+        <div className={`Modal__group ${ formValid.note && 'Modal__group-error' }`}>
           <textarea
+            id="notes"
             type="text"
-            className="form-control"
-            placeholder="Notas"
-            rows="5"
+            className="Modal__input"
+            placeholder=" "
+            rows="2"
             name="notes"
             value={ formValues.notes }
             onChange={ handleInputChange }
-          ></textarea>
-          <small id="emailHelp" className="form-text text-muted">
+          >
+          </textarea>
+          <label htmlFor="notes" className="Modal__label">
             Información adicional
-          </small>
+          </label>
         </div>
 
-        <button type="submit" className="btn btn-outline-primary btn-block">
+        <button type="submit" className="btn ">
           <i className="far fa-save"></i>
           <span> Guardar</span>
         </button>
